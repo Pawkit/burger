1dbfd827176d23e0ec4644b8ef93a07e747f3fbd
Patrick <patrickawong@gmail.com>
2017-11-17 20:52:24 -0800

    working yay

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
-//Dependencies
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
-var exphbs = require("express-handlebars");
 
-
+// Sets up the Express App
+// =============================================================
 var app = express();
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public"));
+// Requiring our models for syncing
+var db = require("./models");
 
-// Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+// Static directory
+app.use(express.static("./public"));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// Routes =============================================================
 
-var routes = require("./controllers/burgers_controller.js");
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
 
-app.use("/" , routes);
-
-app.listen(port);
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
